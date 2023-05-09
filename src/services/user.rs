b5cddf092f56532e604c2387fff270af1ed9cd63
Hugo Martineau <hugo.martineau3@gmail.com>
2023-05-09 10:01:25 +0200

    first satisfying version testing

--- src/services/user.rs
+++ src/services/user.rs
@@ -1,5 +1,9 @@
+use std::sync::Arc;
 use tokio::io::AsyncReadExt;
 use tokio::io::AsyncWriteExt;
+use tokio::net::tcp::OwnedReadHalf;
+use tokio::net::tcp::OwnedWriteHalf;
+use tokio::sync::Mutex;
 
 /// ### Read the stdin and sends the result in string
 async fn read_stdin() -> std::io::Result<String> {
@@ -13,7 +17,7 @@ async fn read_stdin() -> std::io::Result<String> {
 ///
 /// ## Arguments
 /// * socket_output : socket where to read the output and write to stdout
-async fn read_socket_output(
+pub async fn read_socket_output(
     mut socket_output: tokio::net::tcp::OwnedReadHalf,
 ) -> std::io::Result<()> {
     // Loop
@@ -38,29 +42,42 @@ async fn read_socket_output(
 ///
 /// ## Arguments
 /// * socket_input : socket where to write the output of the stdin
-async fn read_stdin_output(
-    mut socket_input: tokio::net::tcp::OwnedWriteHalf,
+pub async fn read_stdin_output(
+    socket_input: tokio::net::tcp::OwnedWriteHalf,
 ) -> std::io::Result<()> {
+    let arc_socket_input = Arc::new(Mutex::new(socket_input));
     // Loop
     loop {
         // Read stdin to become socket input
         let input = read_stdin().await?;
-        // Retrieve size of stdin
-        let size_in_byte: [u8; 8] = input.len().to_ne_bytes();
-        // Write size
-        socket_input.write_all(&size_in_byte).await?;
-        // Write stdin output
-        socket_input.write_all(input.as_bytes()).await?;
+        let clone = arc_socket_input.clone();
+        let _task = write_socket_input(input, clone).await;
     }
+}
+
+pub async fn write_socket_input(
+    input: String,
+    arc_socket_input: Arc<Mutex<tokio::net::tcp::OwnedWriteHalf>>,
+) -> tokio::io::Result<()> {
+    let mut socket_input = arc_socket_input.lock().await;
+    // Write size
+    socket_input.write_all(&input.len().to_ne_bytes()).await?;
+    // Write stdin output
+    socket_input.write_all(input.as_bytes()).await?;
+    drop(socket_input);
+    // Returns ok behavior
+    return Ok(());
+}
+
+pub async fn connect() -> Result<(OwnedReadHalf, OwnedWriteHalf), Box<dyn std::error::Error>> {
+    let socket = tokio::net::TcpStream::connect("127.0.0.1:8080").await?;
+    println!("TCP Stream connected");
+    return Ok(socket.into_split());
 }
 
 /// ### Start the client who tries to connect and create two task for reading socket and stdin
 pub async fn start() -> Result<(), Box<dyn std::error::Error>> {
-    // Create client socket
-    let socket = tokio::net::TcpStream::connect("127.0.0.1:8080").await?;
-    println!("TCP Stream connected");
-    // Retrieve input and output of the socket
-    let (output, input) = socket.into_split();
+    let (output, input) = connect().await?;
     // Create task reading output from stdin
     let task_read_user_input = tokio::task::spawn(read_stdin_output(input));
     // Create task reading output from socket
